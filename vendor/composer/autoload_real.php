5781b7c951c4d7d17d2a17d4c5bada3e364ee6c9
Tomas <jurnicek.tomas@gmail.com>
2018-03-20 00:14:20 +0100

    datagrid added, treeview for case sets is started

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,15 +23,35 @@ class ComposerAutoloaderInit4e76a5ed7d99f3187ed4a44de0aa3cbc
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit4e76a5ed7d99f3187ed4a44de0aa3cbc', 'loadClassLoader'));
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit4e76a5ed7d99f3187ed4a44de0aa3cbc::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit4e76a5ed7d99f3187ed4a44de0aa3cbc::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire4e76a5ed7d99f3187ed4a44de0aa3cbc($fileIdentifier, $file);
         }
